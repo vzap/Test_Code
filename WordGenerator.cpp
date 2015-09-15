99c1e2b1f76b28f0396a3cfedd3dc9836e353e73
KevL159357 <kl1983@nyu.edu>
2015-09-15 19:58:22 -0400

    RandomWordGenerator
    Using basic alphabet and number generate words

---
+++ WordGenerator.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int genRand(int min, int max){
+
+	int range = max - min + 1;
+	return( rand() % range + min);
+		
+}
+
+
+void storeWords(fstream& myFile, int num,int min, int max, string& alphabet){
+	int ranNum;
+	string word = "";
+	for(int i = 0; i < num; ++i){
+		ranNum = genRand(min,max);
+		for(int x = 0; x < ranNum; ++x){
+			word += alphabet[rand() % 62];
+		}
+		myFile << (word + "\n");
+		word = "";
+	}	
+}
+
+void characterFill(string& holder){
+	for(char i = 'a'; i <= 'z'; ++i){
+		holder += i;
+	}
+	for(char i = 'A'; i <= 'Z'; ++i){
+		holder += i;
+	}
+	for(int x = 0; x < 10; ++x){
+		holder += to_string(x);
+	}
+}
+int main(){
+	string text;
+	fstream myfile;
+	myfile.open("GenWords.txt", fstream::out | fstream::app);
+		
+	while(!myfile.eof()){
+	myfile >> text;
+	cout << text << endl;
+	}
+
+
+
+	myfile.close();
+	system("pause");
+}
